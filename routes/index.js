48f04672ec6e8d4e187f5f5ed0de799022686c1e
ron981010 <rcacarapi.98@gmail.com>
2023-07-18 11:48:31 -0400

    update routes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/users', require('./users'));
+router.use('/poems', require('./poems'));
+router.use('/ratings', require('./ratings'));
+router.use('/favorites', require('./favorites'));
+
 
 module.exports = router;
