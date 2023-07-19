1d4977489b56486001ad26673a5001bfa8b6f636
ron981010 <rcacarapi.98@gmail.com>
2023-07-18 23:37:01 -0400

    poems fixed

--- controllers/poems.js
+++ controllers/poems.js
@@ -20,22 +20,14 @@ const getAllPoems = async (req, res,) => {
 
 // get a single poem from database
 const getPoemById = async (req, res) => {
-  try {
-    if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json('Must use a valid poem id to find a poem.');
-    }
-    const poemId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb().db('').collection('poems')
-      .find({ _id: poemId });
-    result.toArray().then((poems) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(poems[0]);
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  const poemId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('poems').find({ _id: poemId });
+  result.toArray().then((poems) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(poems[0]);
+  });
 };
+
 
 // create new poem
 const createPoem = async (req, res) => {
@@ -44,25 +36,20 @@ const createPoem = async (req, res) => {
       poem_id: req.body.poem_id,
       title: req.body.title,
       content: req.body.content,
-      author_id: req.body.author_id, 
-      author: req.body.author, 
-      date_created: req.body.date_created, 
-      tags: req.body.tags 
+      author_id: req.body.author_id,
+      author: req.body.author,
+      date_created: req.body.date_created,
+      tags: req.body.tags
     };
 
-    const response = await mongodb
-      .getDb().db('').collection('poems')
-      .insertOne(poem);
+    const response = await mongodb.getDb().db().collection('poems').insertOne(poem);
     if (response.acknowledged) {
-      console.log('Created successfully');
-      res.setHeader('Content-Type', 'application/json');
       res.status(201).json(response);
     } else {
-      res
-        .status(500)
-        .json(response.error || 'An error occurred while creating the poem!');
+      res.status(500).json(response.error || 'Some error occurred while creating the poem.');
     }
-  } catch (err) {
+  }
+  catch (err) {
     console.log(err);
     res.status(500).json({ error: err });
   }
@@ -79,23 +66,17 @@ const updatePoem = async (req, res) => {
       poem_id: req.body.poem_id,
       title: req.body.title,
       content: req.body.content,
-      author_id: req.body.author_id, 
-      author: req.body.author, 
-      date_created: req.body.date_created, 
-      tags: req.body.tags 
+      author_id: req.body.author_id,
+      author: req.body.author,
+      date_created: req.body.date_created,
+      tags: req.body.tags
     };
-    const response = await mongodb
-      .getDb().db('').collection('poems')
-      .replaceOne({ _id: poemId }, poem);
+    const response = await mongodb.getDb().db().collection('poems').replaceOne({ _id: poemId }, poem);
     console.log(response);
-    if (response.modifiedCount === 1) {
-      console.log('Updated successfully');
-      res.setHeader('Content-Type', 'application/json');
-      res.status(204).json(response).send;
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
     } else {
-      res
-        .status(500)
-        .json(response.error || 'An error occurred while updating the poem!');
+      res.status(500).json(response.error || 'Some error occurred while updating the favorite poem.');
     }
   } catch (err) {
     console.log(err);
@@ -106,21 +87,13 @@ const updatePoem = async (req, res) => {
 // delete poem by id in the database
 const deletePoem = async (req, res) => {
   try {
-    if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json('Must use a valid poem id to delete a poem.');
-    }
     const poemId = new ObjectId(req.params.id);
-    const response = await mongodb
-      .getDb().db('').collection('poems')
-      .deleteOne({ _id: poemId });
-    if (response.deletedCount === 1) {
-      console.log('Deleted successfully');
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(response).send;
+    const response = await mongodb.getDb().db().collection('poems').remove({ _id: poemId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
     } else {
-      res
-        .status(500)
-        .json(response.error || 'An error occurred while deleting the poem!');
+      res.status(500).json(response.error || 'Some error occurred while deleting the poem from the user favorite list.');
     }
   } catch (err) {
     console.log(err);
